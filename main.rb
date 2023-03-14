7c6da4f957177ed96e3d31a5348141c31599eed4
Juanpa8830 <jp3_4@hotmail.com>
2023-03-14 13:03:58 -0500

    Add main file to test code

---
+++ main.rb
@@ -0,0 +1,10 @@
+require_relative 'person'
+require_relative 'capitalize_decorator'
+require_relative 'trim_decorator'
+
+puts person = Person.new(22, 'maximilianus')
+puts person.correct_name
+puts capitalized_person = CapitalizeDecorator.new(person)
+puts capitalized_person.correct_name
+puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
+puts capitalized_trimmed_person.correct_name
