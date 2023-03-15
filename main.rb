5f26081035f2d4aee18d0106d33b9851c51cd9b8
Juanpa8830 <jp3_4@hotmail.com>
2023-03-15 13:31:50 -0500

    Add some test to check code

--- main.rb
+++ main.rb
@@ -1,10 +1,30 @@
-require_relative 'person'
-require_relative 'capitalize_decorator'
-require_relative 'trim_decorator'
+require_relative 'classroom'
+require_relative 'student'
+require_relative 'rental'
+require_relative 'book'
 
-puts person = Person.new(22, 'maximilianus')
-puts person.correct_name
-puts capitalized_person = CapitalizeDecorator.new(person)
-puts capitalized_person.correct_name
-puts capitalized_trimmed_person = TrimmerDecorator.new(capitalized_person)
-puts capitalized_trimmed_person.correct_name
+math = Classroom.new('Matematicas')
+martha = Student.new(20, 'Martha')
+pedro = Student.new(15, 'Pedro')
+
+
+pedro.classroom = math
+martha.classroom = math
+
+puts math.students.map(&:name)
+
+puts martha.classroom.label
+puts math.label
+
+person_uno = Person.new(20, 'alex')
+libro = Book.new('la vida es bella', 'Pierre')
+rental_uno = Rental.new('2023-03-15')
+
+libro.add_rentals(rental_uno)
+puts libro.rentals.map(&:date)
+rental_uno.book = libro
+puts libro.rentals.map(&:date)
+person_uno.add_rentals(rental_uno)
+
+rental_uno.person = person_uno
+puts person_uno.rentals.map(&:date)
