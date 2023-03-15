a98e5703ef2398aadb0a2b94df67053c7f88cd55
Juanpa8830 <jp3_4@hotmail.com>
2023-03-15 13:33:12 -0500

    Refactor class Person and its relationships

--- person.rb
+++ person.rb
@@ -4,7 +4,7 @@ require_relative 'nameable'
 
 class Person < Nameable
   attr_accessor :name, :age
-  attr_reader :id
+  attr_reader :id, :rentals
 
   def initialize(age, name = 'Unknown', parent_permission: true)
     super()
@@ -12,6 +12,7 @@ class Person < Nameable
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
   end
 
   def correct_name
@@ -20,6 +21,11 @@ class Person < Nameable
 
   def can_use_services?
     of_age? || @parent_permission == true
+  end
+
+  def add_rentals(rental)
+    rentals.push(rental)
+    rental.person = self
   end
 
   private
