a6ee5dbcf960ae5847f41144832b4bd62cf37f56
Juanpa8830 <jp3_4@hotmail.com>
2023-03-16 21:00:06 -0500

    Add executable file

--- main.rb
+++ main.rb
@@ -1,16 +1,28 @@
 require_relative 'classroom'
 require_relative 'student'
+require_relative 'teacher'
 require_relative 'rental'
 require_relative 'book'
+require_relative 'person'
+require_relative 'app'
 
-person_uno = Student.new(20, 'francisco')
-person_dos = Student.new(18, 'Ana')
-libro_uno = Book.new('hello World', 'author hello')
-libro_dos = Book.new('hello Moon', 'meeeeeee')
-renta_uno = Rental.new('2022-03-15', libro_uno, person_uno)
-renta_dos = Rental.new('2022-03-15', libro_dos, person_uno)
+# book_uno = Book.new("hola amigos", "Author1")
+# book_dos = Book.new("adios señores", "Author2")
+# book_tres = Book.new("y entocnes?", "Author3")
+#
+# person_uno = Student.new(16, "Pierre")
+# person_dos = Teacher.new(16, "francisco")
+# person_tres = Student.new(16, "Juan")
+#
+# rental_uno = Rental.new('2018-05-20', book_uno, person_uno)
+# rental_dos = Rental.new('2019-06-22', book_dos, person_dos)
+# rental_tres = Rental.new('2022-08-30', book_tres, person_tres)
+# puts book_uno.rental_list
 
-person_uno.rentals.each{|rental| p [rental.book.title, rental.person.name] }
+def main
+  app = App.new
+  app.start
+  puts 'Thanks for using this app!'
+end
 
-
-
+main
