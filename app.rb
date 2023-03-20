352c2e13505d68e1b43526a6db0cd248acb3af9a
Juanpa8830 <jp3_4@hotmail.com>
2023-03-20 12:55:16 -0500

    Refactor main file to include menu_options

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@ require_relative 'teacher'
 require_relative 'book'
 require_relative 'rental'
 require_relative 'person'
+require_relative 'prueba'
 
 class App
   def initialize
@@ -62,20 +63,6 @@ class App
     puts 'Press any key to continue...'
     gets.chomp
     puts ''
-  end
-
-  def selected_menu_option
-    puts '@@@@@@@@@@ SCHOOL LIBRARY APP @@@@@@@@@@@'
-    puts "\nPlease choose an option by entering a number: "
-    puts '1 - List all books.'
-    puts '2 - List all people.'
-    puts '3 - Create a person,'
-    puts '4 - Create a book.'
-    puts '5 - Create a rental.'
-    puts '6 - List all rentals for a given person id.'
-    puts '7 - Exit'
-
-    get_option_selected(1, 7)
   end
 
   def create_book
--- main.rb
+++ main.rb
@@ -1,15 +1,23 @@
-require_relative 'classroom'
-require_relative 'student'
-require_relative 'teacher'
-require_relative 'rental'
-require_relative 'book'
-require_relative 'person'
 require_relative 'app'
 
 def main
   app = App.new
   app.start
   puts 'Thanks for using this app!'
+end
+
+def selected_menu_option
+  puts '@@@@@@@@@@ SCHOOL LIBRARY APP @@@@@@@@@@@'
+  puts "\nPlease choose an option by entering a number: "
+  puts '1 - List all books.'
+  puts '2 - List all people.'
+  puts '3 - Create a person,'
+  puts '4 - Create a book.'
+  puts '5 - Create a rental.'
+  puts '6 - List all rentals for a given person id.'
+  puts '7 - Exit'
+
+  get_option_selected(1, 7)
 end
 
 main
